1f08118642280c07245337f70ab17830975d1ced
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-13 13:49:51 -0400

    Initial Commit

---
+++ BarcodeScannerSwiftUI/BarcodeScannerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerSwiftUIApp.swift
+//  BarcodeScannerSwiftUI
+//
+//  Created by Oscar Cristaldo on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
